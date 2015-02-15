c6941476bd0a6a5c52f4205d66ffa62684a9e1b6
Dustin McQuay <dmcquay@gmail.com>
2015-02-14 21:29:13 -0700

    Automatically install ntp

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
 echo deb https://get.docker.com/ubuntu docker main > /etc/apt/sources.list.d/docker.list
 apt-get update
-apt-get install -y lxc-docker mysql-client-core-5.6 pv
+apt-get install -y lxc-docker mysql-client-core-5.6 pv ntp
 
 # install fig
 curl -L https://github.com/docker/fig/releases/download/1.0.1/fig-`uname -s`-`uname -m` > /usr/local/bin/fig; chmod +x /usr/local/bin/fig
